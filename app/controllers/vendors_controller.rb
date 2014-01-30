037efdcf727c6511eedaf9f2baf0c04bda48bb32
Alan Fung-Schwarz <alan@atomsign.net>
2014-01-30 12:09:54 -0800

    Green test for create action

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,13 @@
 class VendorsController < ApplicationController
+  respond_to :js
+
+  def create
+    @vendor = Vendor.create(permitted_params)
+    redirect_to vendor_path(@vendor)
+  end
+
+  private
+  def permitted_params
+    params.require(:vendor).permit(:name)
+  end
 end
