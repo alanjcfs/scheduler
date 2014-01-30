6c87159745483a2fae8a0cab075307f78fec6893
Alan Fung-Schwarz <alan@atomsign.net>
2014-01-30 12:04:24 -0800

    Test that post create a vendor

--- test/controllers/vendors_controller_test.rb
+++ test/controllers/vendors_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class VendorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post to Vendor creates a new vendor" do
+    assert_difference('Vendor.count') do
+      post :create, vendor: { name: "Joe the Surf Instructor" }
+    end
+
+    assert_redirected_to vendor_path(assigns(:vendor))
+  end
 end
