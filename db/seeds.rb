044b5b1872491709297ef9bedc4bc503420c21d3
Alan Fung-Schwarz <alan@atomsign.net>
2014-01-31 15:16:06 -0800

    Add seeds for vendors and activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+vendors = [{name: "Joe the Surfer"},
+           {name: "Sarah the Snowboarder"},
+           {name: "Sam the Racer"}]
+
+vendors = Vendor.create(vendors)
+
+vendors.each do |vendor|
+  vendor.activities.create(name: "#{vendor.name}'s Lesson")
+end
+
+Activity.all.each do |activity|
+  activity.schedules.create(date: 2.days.ago,
+                           time_start: Time.parse("12:00:00"),
+                           time_end: Time.parse("14:00:00"),
+                           quantity: 8)
+end
